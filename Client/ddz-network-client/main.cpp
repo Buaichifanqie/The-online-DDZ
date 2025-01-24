8947fbbee0cfc6286a7100179e61a59c410c0c6a
kongwenshuo <2806144405@qq.com>
2025-01-25 00:25:50 +0800

    在客户端完成了非对称加密RSA类的封装和Base64类的封装

--- Client/ddz-network-client/main.cpp
+++ Client/ddz-network-client/main.cpp
@@ -1,6 +1,7 @@
 #include "cards.h"
 #include "login.h"
 
+#include "rsacrypto.h"
 #include <QApplication>
 #include <QResource>
 #include "aescrypto.h"
@@ -8,10 +9,31 @@
 
 void test()
 {
-    AesCrypto aes(AesCrypto::AES_CBC_128,"1234567887654321");
-    QByteArray text=aes.encrypt("奥奇传说启动!!!");
-    text=aes.decrypt(text);
-    qDebug()<<text.data();
+    AesCrypto aes(AesCrypto::AES_CBC_128, "1234567887654321");
+    QByteArray text = aes.encrypt("奥奇传说启动！！！");
+    text = aes.decrypt(text);
+    qDebug() << text.data();
+
+    RsaCrypto rsa;
+    rsa.generateRsaKey(RsaCrypto::BITS_2k);
+
+    RsaCrypto rsa1("public.pem", RsaCrypto::PublicKey);
+    // 可加密的最大数据长度 = bits/8-11;  11是填充
+    QByteArray temp = "我要玩奥奇传说！！！";
+    temp = rsa1.pubKeyEncrypt(temp);
+    qDebug() << "公钥加密的密文: " << temp;
+
+    RsaCrypto rsa2("private.pem", RsaCrypto::PrivateKey);
+    temp = rsa2.priKeyDecrypt(temp);
+    qDebug() << temp.data();
+
+
+    temp = rsa2.sign(text);
+    qDebug() << "私钥签名的密文: " << temp;
+
+    bool flag = rsa1.verify(temp, text);
+    qDebug() << "flag = " << flag;
+
 }
 
 
